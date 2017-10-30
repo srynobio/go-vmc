f366c56ca98fa5e150e0e8286dd1b6d1f8d29969
shawn rynearson <shawn.rynearson@gmail.com>
2017-10-30 16:12:22 -0600

    weekly updates to code base

--- gofastq-vmc.go
+++ gofastq-vmc.go
@@ -16,12 +16,10 @@ const goUsage = `
 
 Required options:
 
-		-fasta	: Fasta file to used to create VMC based ID.
+	-fasta	   : Fasta file to used to create VMC based ID.
 
-		-nameSpace :	Namespace accession authority 
-						Examples:
-							NCBI
-							Ensembl
+	-nameSpace :	Namespace accession authority 
+					Examples: NCBI, ENsembl, UCSC
 
 Additional options:
 
